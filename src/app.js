ca98c179d6e8a72a06158d2c4714937907546589
Melissa Castorena <mcastorena0316@gmail.com>
2020-03-23 16:59:59 -0600

    create input for players name and markers

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 const UI = () => {
   const displaySection = (clickedBtn, sectionToShow) => {
-    let allSections = document.querySelectorAll('.wrapper section');
-    let clickedButton = document.getElementById(clickedBtn);
-    clickedButton.addEventListener('click', function (e) {
-      for (let i = 0; i < allSections.length; i++) {
-        let id = allSections[i].id;
+    const allSections = document.querySelectorAll('.wrapper section');
+    const clickedButton = document.getElementById(clickedBtn);
+    clickedButton.addEventListener('click', (e) => {
+      for (let i = 0; i < allSections.length; i += 1) {
+        const { id } = allSections[i];
         if (id === sectionToShow) {
           allSections[i].style.display = 'block';
         } else {
@@ -13,25 +13,71 @@ const UI = () => {
       }
 
       e.preventDefault();
-    })
-  }
+    });
+  };
 
   const displayContinueToGame = () => {
     displaySection('start-btn', 'continue-to-game');
-  }
+  };
 
   const displayInstruction = () => {
     displaySection('read-instruction-btn', 'read-instruction');
-  }
+  };
 
   const returnToContinueToGame = () => {
     displaySection('return-to-continue-btn', 'continue-to-game');
-  }
+  };
 
-  return { displayContinueToGame, displayInstruction, returnToContinueToGame }
-}
+  const displayStartGame = () => {
+    displaySection('playgame-btn', 'playgame-section');
+  };
+
+  const displayBoard = () => {
+    const validateform = document.getElementById('set-player-btn');
+    validateform.addEventListener('click', validateData);
+    function validateData(e) {
+
+      const playersInput = document.querySelectorAll('.playgame-section .form-control');
+      const playersEmptyInput = Array.from(playersInput).filter((item) => {
+        return item.value === '';
+      });
+      function checkMarker(marker) {
+        if (marker.value === 'X') {
+          document.getElementById('player2-marker').value = 'O';
+          document.getElementById('boardgame-section').style.display = 'block';
+          document.getElementById('playgame-section').style.display = 'none';
+        } else if (marker.value === 'O') {
+          document.getElementById('player2-marker').value = 'X';
+          document.getElementById('boardgame-section').style.display = 'block';
+          document.getElementById('playgame-section').style.display = 'none';
+        } else {
+          alert("Select a valid marker")
+        }
+      }
+
+      if (playersEmptyInput.length > 0) {
+        alert("Fill all your inputs");
+      } else {
+        const marker1 = document.getElementById('player1-marker');
+        marker1.addEventListener('keyup', checkMarker(marker1));
+      }
+
+      e.preventDefault();
+    }
+  };
+
+  return {
+    displayContinueToGame,
+    displayInstruction,
+    returnToContinueToGame,
+    displayStartGame,
+    displayBoard,
+  };
+};
 
 const ui = UI();
 ui.displayContinueToGame();
 ui.displayInstruction();
 ui.returnToContinueToGame();
+ui.displayStartGame();
+ui.displayBoard();
