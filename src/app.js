f1f9d816dbe1abf6edaa01fb443b792cee043437
MaryAnn Chukwuka <annychuks07@gmail.com>
2020-03-25 19:01:14 +0100

    refactor game object methods

--- src/app.js
+++ src/app.js
@@ -33,9 +33,16 @@ const UI = () => {
     displaySection('playgame-btn', 'playgame-section');
   };
 
+  const showErrorMsg = (errorEle, errorClass, errorMsg) => {
+    document.querySelector(errorEle).innerHTML = `<span class="text-danger" id="${errorClass}">${errorMsg}</span>`;
+
+    setTimeout(() => {
+      document.querySelector(`#${errorClass}`).remove();
+    }, 2000);
+  };
+
   const checkMarker = () => {
     const marker1 = document.getElementById('player1-marker');
-
     function inputChecker() {
       const x = ['X', 'x'];
       const o = ['O', 'o'];
@@ -46,7 +53,7 @@ const UI = () => {
       } else if (marker1.value === '') {
         document.getElementById('player2-marker').value = '';
       } else {
-        alert('Select a valid marker');
+        showErrorMsg('.game-error-msg', 'error', 'Input either X or O');
       }
     }
     marker1.addEventListener('keyup', inputChecker);
@@ -63,6 +70,7 @@ const UI = () => {
     displayStartGame,
     checkMarker,
     exitGame,
+    showErrorMsg,
   };
 };
 
@@ -79,20 +87,23 @@ const Player = (name, marker) => {
 
 
 const gameBoard = (() => {
-  const showBoard = () => {
+  const renderBoard = () => {
     let cell = '';
     for (let i = 1; i <= 9; i += 1) {
       cell += `<button class="cell cell-${i}" id="cell-${i}"></button>`;
     }
     document.getElementById('display-board').innerHTML = cell;
   };
-
-  return { showBoard };
+  return { renderBoard };
 })();
 
-const TicTacToe = () => {
+const Game = () => {
   const winningCombinations = () => {
-    const winningArray = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
+    const winningArray = [
+      [1, 2, 3], [4, 5, 6], [7, 8, 9],
+      [1, 4, 7], [2, 5, 8], [3, 6, 9],
+      [1, 5, 9], [3, 5, 7],
+    ];
     return winningArray;
   };
 
@@ -103,67 +114,68 @@ const TicTacToe = () => {
     return Array.from(boardsChecked).every((element) => element.innerText !== '');
   };
 
-  const displayBoard = () => {
+  const getEleValue = (eleId) => document.getElementById(eleId).value;
+
+  const handleCellSwapTurns = (player1, player2) => {
+    const markerOnBoard = document.querySelectorAll('.cell');
+    const changeName = document.getElementById('get-turn-msg');
+    let currentPlayer = player1;
+    changeName.innerHTML = `${currentPlayer.getName()}, is your turn!`;
+    markerOnBoard.forEach((element) => {
+      element.addEventListener('click', (e) => {
+        element.innerHTML = currentPlayer.getMarker();
+        element.disabled = true;
+
+        if (checkWinner(currentPlayer.getMarker())) {
+          changeName.innerHTML = `Congratulations, ${currentPlayer.getName()}, you won the game!`;
+          currentPlayer.increaseScore();
+          document.querySelector(`.${currentPlayer.getMarker()}`).innerText = `Score: ${currentPlayer.getScore()}`;
+          markerOnBoard.forEach((ele) => { ele.disabled = true; });
+        } else {
+          currentPlayer = currentPlayer === player1 ? player2 : player1;
+          changeName.innerHTML = `${currentPlayer.getName()}, is your turn!`;
+          if (tieMove()) {
+            changeName.innerHTML = 'The game is a draw';
+          }
+        }
+
+        e.preventDefault();
+      });
+    });
+  };
+
+  const handleClickedSubmitPlayer = () => {
     function validateData(e) {
       const playersInput = document.querySelectorAll('.playgame-section .form-control');
       const playersEmptyInput = Array.from(playersInput).filter((item) => item.value === '');
       if (playersEmptyInput.length > 0) {
-        alert('Fill all your inputs');
+        const ui = UI();
+        ui.showErrorMsg('.game-error-msg', 'error-inputs', 'Please fill all inputs');
       } else {
-        gameBoard.showBoard();
+        gameBoard.renderBoard();
         document.getElementById('boardgame-section').style.display = 'block';
         document.getElementById('playgame-section').style.display = 'none';
-        const playerOneName = document.getElementById('player1-name').value;
-        const playerOneMarker = document.getElementById('player1-marker').value;
-        const playerTwoName = document.getElementById('player2-name').value;
-        const playerTwoMarker = document.getElementById('player2-marker').value;
-        document.querySelector('#player1-score').classList.add(playerOneMarker);
-        document.querySelector('#player2-score').classList.add(playerTwoMarker);
-        const player1 = Player(playerOneName, playerOneMarker);
-        const player2 = Player(playerTwoName, playerTwoMarker);
+
+        const player1 = Player(getEleValue('player1-name'), getEleValue('player1-marker'));
+        const player2 = Player(getEleValue('player2-name'), getEleValue('player2-marker'));
 
         const playersInputs = document.querySelectorAll('#boardgame-section .form-control');
         Array.from(playersInputs).forEach((ele) => {
           const { id } = ele;
-          // eslint-disable-next-line no-param-reassign
           ele.innerText = document.querySelector(`.playgame-section #${id}`).value;
         });
 
-        const markerOnBoard = document.querySelectorAll('.cell');
-        let currentPlayer = player1;
-        const changeName = document.getElementById('get-turn-msg');
-        changeName.innerHTML = `${currentPlayer.getName()}, is your turn!`;
-        markerOnBoard.forEach((element) => {
-          element.addEventListener('click', (e) => {
-            // eslint-disable-next-line no-param-reassign
-            element.innerHTML = currentPlayer.getMarker();
-            element.disabled = true;
-
-            if (checkWinner(currentPlayer.getMarker())) {
-              changeName.innerHTML = `Congratulations, ${currentPlayer.getName()}, you won the game!`;
-              currentPlayer.increaseScore();
-              document.querySelector(`.${currentPlayer.getMarker()}`).innerText = `Score: ${currentPlayer.getScore()}`;
-              markerOnBoard.forEach((ele) => {
-                ele.disabled = true;
-              });
-            } else {
-              currentPlayer = currentPlayer === player1 ? player2 : player1;
-              changeName.innerHTML = `${currentPlayer.getName()}, is your turn!`;
-              if (tieMove()) {
-                changeName.innerHTML = 'The game is a draw';
-              }
-            }
-
-
-            e.preventDefault();
-          });
-        });
+        handleCellSwapTurns(player1, player2);
       }
 
       e.preventDefault();
     }
+    return validateData;
+  };
+
+  const playGame = () => {
     const validateform = document.getElementById('set-player-btn');
-    validateform.addEventListener('click', validateData);
+    validateform.addEventListener('click', handleClickedSubmitPlayer());
   };
 
   const playAgain = () => {
@@ -180,7 +192,7 @@ const TicTacToe = () => {
   };
 
 
-  return { displayBoard, playAgain };
+  return { playGame, playAgain };
 };
 
 const ui = UI();
@@ -191,6 +203,6 @@ ui.displayStartGame();
 ui.checkMarker();
 ui.exitGame();
 
-const tictactoe = TicTacToe();
-tictactoe.displayBoard();
-tictactoe.playAgain();
+const game = Game();
+game.playGame();
+game.playAgain();
