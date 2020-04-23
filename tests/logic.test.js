72ac82e68c515b4a8ccc33f017b2d6805afa6d66
MaryAnn Chukwuka <annychuks07@gmail.com>
2020-04-23 04:10:42 +0100

    refactor test functions and add two more test

--- tests/logic.test.js
+++ tests/logic.test.js
@@ -19,52 +19,87 @@ test('should check for non empty space in all cells', () => {
   for (let i = 5; i <= 9; i += 1) {
     logic.placeMarker(i, 'O');
   }
-
   const notEmpty = logic.boardArray.every((item) => item !== '');
-
   expect(notEmpty).toBeTruthy();
+  logic.resetGame();
+});
+
+test('should check for filled cells', () => {
+  [1, 2, 4].forEach((item) => {
+    logic.placeMarker(item, 'X');
+  });
+
+  [3, 6].forEach((item) => {
+    logic.placeMarker(item, 'O');
+  });
+
+  expect(logic.getFilledCell()).toStrictEqual([1, 2, 3, 4, 6]);
+  logic.resetGame();
 });
 
 test('should reset the game', () => {
-  const emptyArray = logic.resetGame();
-  expect(emptyArray).toStrictEqual(['', '', '', '', '', '', '', '', '']);
+  [3, 6, 8, 7].forEach((item) => {
+    logic.placeMarker(item, 'O');
+  });
+
+  logic.resetGame();
+  expect(logic.isEmpty()).toBe(true);
 });
 
-test('should change turns, starting from Player1 and then switching to player2', () => {
+test('should change turns, starting from player1 and then switching to player2', () => {
   let currentPlayer = player1;
   currentPlayer = logic.changeTurns(currentPlayer, player1, player2);
   expect(currentPlayer).toBe(player2);
 });
 
-test(' player 1 should be the winner ', () => {
-  logic.placeMarker(1, 'X');
-  logic.placeMarker(2, 'X');
-  logic.placeMarker(3, 'X');
+test('player 1 should be the winner ', () => {
+  for (let i = 1; i <= 3; i += 1) {
+    logic.placeMarker(i, 'X');
+  }
   expect(logic.thereIsWinner(player1.getMarker())).toBeTruthy();
 });
 
 test('player 2 should not be the winner', () => {
-  logic.placeMarker(1, 'X');
-  logic.placeMarker(2, 'O');
-  logic.placeMarker(3, 'X');
-  logic.placeMarker(4, 'X');
-  logic.placeMarker(5, 'X');
-  logic.placeMarker(6, 'O');
-  logic.placeMarker(7, 'O');
-  logic.placeMarker(8, 'X');
-  logic.placeMarker(9, '0');
+  [1, 3, 4, 5, 8].forEach((item) => {
+    logic.placeMarker(item, 'X');
+  });
+
+  [2, 6, 7, 9].forEach((item) => {
+    logic.placeMarker(item, 'X');
+  });
   expect(logic.thereIsWinner(player2.getMarker())).toBeFalsy();
 });
 
+test('check if a player won', () => {
+  // By row
+  [1, 2, 3].forEach((item) => {
+    logic.placeMarker(item, 'O');
+  });
+  expect(logic.thereIsWinner(player2.getMarker())).toBeTruthy();
+  logic.resetGame();
+
+  // By column
+  [2, 5, 8].forEach((item) => {
+    logic.placeMarker(item, 'X');
+  });
+  expect(logic.thereIsWinner(player1.getMarker())).toBeTruthy();
+  logic.resetGame();
+
+  // By diagonal
+  [3, 5, 7].forEach((item) => {
+    logic.placeMarker(item, 'X');
+  });
+  expect(logic.thereIsWinner(player1.getMarker())).toBeTruthy();
+  logic.resetGame();
+});
+
 test('should be a tie', () => {
-  logic.placeMarker(1, 'X');
-  logic.placeMarker(2, 'O');
-  logic.placeMarker(3, 'X');
-  logic.placeMarker(4, 'X');
-  logic.placeMarker(5, 'X');
-  logic.placeMarker(6, 'O');
-  logic.placeMarker(7, 'O');
-  logic.placeMarker(8, 'X');
-  logic.placeMarker(9, '0');
+  [1, 3, 4, 5, 8].forEach((item) => {
+    logic.placeMarker(item, 'X');
+  });
+
+  [2, 6, 7, 9].forEach((item) => {
+    logic.placeMarker(item, 'X');
+  });
   expect(logic.tieMove()).toBeTruthy();
 });
